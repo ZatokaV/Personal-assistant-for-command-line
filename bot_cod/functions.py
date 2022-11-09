50dbee87632424f0f58a46cf89f1244eae085a49
unknown <zatokav@gmail.com>
2022-11-10 01:24:12 +0200

    Update functions.py
    add empty functions

---
+++ bot_cod/functions.py
@@ -0,0 +1,54 @@
+# Створення контакту
+def create_contact():
+    pass
+
+
+# Додавання телефону
+def add_phone():
+    pass
+
+
+# Додавання адреси
+def add_adress():
+    pass
+
+
+# додавання імейлу
+def add_email():
+    pass
+
+
+# додавання дня народження
+def add_birthday():
+    pass
+
+
+# виводити список контактів, у яких день народження через
+# задану кількість днів від поточної дати;
+def show_bday_names():
+    pass
+
+
+# здійснювати пошук контактів з книги контактів;
+def searcher_people():
+    pass
+
+
+# видаляти записи з книги контактів
+def delete_contact():
+    pass
+
+
+# додавання нотатки
+def add_note():
+    pass
+
+
+# здійснювати пошук нотаток
+def searcher_notes():
+    pass
+
+
+# здійснювати сортування нотаток за ключовими словами (тегами)
+def sorting_notes():
+    pass
