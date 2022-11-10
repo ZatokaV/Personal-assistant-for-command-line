37a678fcb728352c30709e48908cb6ed20702a44
unknown <zatokav@gmail.com>
2022-11-10 11:27:17 +0200

    Update main.py

--- bot_cod/main.py
+++ bot_cod/main.py
@@ -1,9 +1,27 @@
-USER_INPUT = {}
+from classes import ADRESS_BOOK
+from functions import hello_message, create_contact
+
+USER_INPUT = {
+    "hello": hello_message,
+    "add": create_contact,
+}
 
 
 def main():
     while True:
-        pass
+        user_message = input("...\n").lower()
+
+        if user_message.endswith(" "):
+            user_message = user_message.rstrip()
+        if not user_message or user_message.startswith(" "):
+            continue
+
+        if user_message in ("good bye", "close", "exit"):
+            print("Good bye!")
+            break
+
+        if user_message in USER_INPUT:
+            USER_INPUT[user_message]()
 
 
 if __name__ == "__main__":
