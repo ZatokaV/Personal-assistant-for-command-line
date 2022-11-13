7aa2927d95cd5dba64d19c8407950ae7289e797c
unknown <zatokav@gmail.com>
2022-11-13 22:51:32 +0200

    Update main.py

--- bot_cod/main.py
+++ bot_cod/main.py
@@ -1,101 +1,11 @@
 from classes import ADDRESS_BOOK, NOTE_BOOK
-from functions import (
-    add_adress,
-    add_birthday,
-    add_email,
-    add_phone,
-    create_contact,
-    delete_contact,
-    edit_contact,
-    edit_note,
-    hello_message,
-    notifications,
-    recreate_contacts,
-    recreate_notes,
-    searcher_notes,
-    searcher_people,
-    show_all,
-    show_bday_names,
-)
-from sorter_files import main_sortuvalka
-from spellchecker import SpellChecker
-
-instructions = """
-Phone book:
-Add a new contact: "add" 
-Add other phone for contact: "phone"
-Add contact address: "address"
-Add e-mail: "email"
-Add date of birth: "birthday"
-Birthday alerts for your contacts:"bdays"
-Edit contact: "edit"
-Show all records: "all"
-Search in records: "search"
-Delet record: "delete"
-
-Note book:
-Operations with notes: "note"
-Search in notes: "findnote"
-
-Files on PC:
-For sorting files on you PC: "sort"
-
-See this message again: "help"
-
-Save and exit: "good bye", "close", "exit"
-"""
-
-
-def get_help():
-    print(instructions)
-
-
-def incorrect_input(user_message):
-    my_functions = list(USER_INPUT.keys())
-    my_functions.extend(["good bye", "close", "exit"])
-    spell = SpellChecker()
-    suggestions = spell.candidates(user_message)
-    if suggestions:
-        for suggest in suggestions:
-            if suggest in my_functions:
-                print(
-                    f"{user_message} is not a right function, maybe you mean: '{suggest}'?"
-                )
-                break
-            if len(user_message) <= 2:
-                print(
-                    f"{user_message} is not a right function, maybe you mean: 'add' or 'all'?"
-                )
-                break
-        else:
-            print(f"Sorry can't execute this function: {user_message}.")
-    else:
-        print(f"Sorry don't know this command: {user_message}")
-
-
-USER_INPUT = {
-    "hello": hello_message,
-    "add": create_contact,
-    "phone": add_phone,
-    "address": add_adress,
-    "email": add_email,
-    "birthday": add_birthday,
-    "all": show_all,
-    "edit": edit_contact,
-    "sort": main_sortuvalka,
-    "note": notifications,
-    "findnote": searcher_notes,
-    "delete": delete_contact,
-    "search": searcher_people,
-    "bdays": show_bday_names,
-    "help": get_help,
-    "noteedit": edit_note,
-}
+from functions import get_help, incorrect_input, recreate_contacts, recreate_notes
+from technical import USER_INPUT, instructions
 
 
 def main():
 
-    print(instructions)
+    get_help(instructions)
     recreate_contacts()
     recreate_notes()
 
@@ -117,7 +27,7 @@ def main():
             USER_INPUT[user_message]()
 
         if user_message not in USER_INPUT:
-            incorrect_input(user_message)
+            incorrect_input(user_message, USER_INPUT)
 
 
 if __name__ == "__main__":
