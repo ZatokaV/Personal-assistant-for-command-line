3c1594ff4585afdf804c5e694c9784513e2f0b86
De1c <denpro8350@gmail.com>
2022-11-13 14:34:19 +0100

    Setup check

---
+++ personal_helper/personal_helper/classes.py
@@ -0,0 +1,194 @@
+from collections import UserDict
+import re
+from datetime import datetime
+from string import ascii_letters
+
+
+class Field:
+    def __init__(self, value) -> None:
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        self.value = [value]
+
+    @staticmethod
+    def phone_validator(verification_number):
+        if len(verification_number) > 17:
+            return False
+        for i in verification_number:
+            if i in ascii_letters:
+                return False
+        return True
+
+
+class Birthday(Field):
+    @staticmethod
+    def birthday_validator(verification_birthday):
+        today = datetime.now()
+        if verification_birthday < datetime.date(today):
+            return True
+        return False
+
+
+class Adress(Field):
+    pass
+
+
+class Email(Field):
+    @staticmethod
+    def email_validator(verification_mail):
+        result = re.findall(
+            r"[a-zA-Z]{1,}[\w\.]{1,}@[a-zA-Z]{2,}.[a-zA-Z]{2,}", verification_mail)
+        if result:
+            return True
+        return False
+
+
+class Tag(Field):
+    pass
+
+
+class NoteText(Field):
+    pass
+
+
+class Notification:
+    def __init__(self, notes: NoteText, tags: Tag = None) -> None:
+        self.tags = tags
+        self.notes = notes
+
+    def change_text(self, new_value):
+        self.notes.value = new_value
+
+    def find_text(self, key_phrase):
+
+        value = self.notes.value
+        match = re.fullmatch(key_phrase, value)
+        if match:
+            return f"Key-phrase:{key_phrase} was found in the {value}"
+        return f"{key_phrase} not found"
+
+    def find_tag(self, tag):
+
+        tags = self.tags
+        if tags:
+            for each_tag in tags:
+                # Перевірка тегу серед всіх тегів
+                match = re.fullmatch(tag, each_tag.value)
+                if match:
+                    return "Tag found"
+                return f"Sorry but there is no tag:{tag}"
+        return "There are no tags at all"
+
+    def add_tags(self, *tags):
+
+        self.tags.extend(list(tags))
+        tags = str(list(tags))
+        clean_tags = re.sub(r"\[|\]", '', tags)
+        return f"{clean_tags} were added"
+
+    def delete_tags(self, input_tag):
+
+        tags = self.tags
+        if tags:
+            try:
+                self.tags.remove(Tag(input_tag))
+                return f"{input_tag} was deleted successfully"
+            except ValueError:
+                return f"Sorry but there is no tag:{input_tag}"
+        return f"There are no tags at all"
+
+
+class Record:
+    def __init__(
+        self,
+        name: Name,
+        phone: Phone,
+        birthday: Birthday = None,
+        adress: Adress = None,
+        email: Email = None,
+    ) -> None:
+        self.name = name
+        self.phone = phone
+        self.birthday = birthday
+        self.adress = adress
+        self.email = email
+
+    def change_name(self):
+        new_name = Name(input("Enter new name\n"))
+        new_record = Record(new_name, self.phone, self.birthday,
+                            self.adress, self.email)
+        ADDRESS_BOOK.add_record(new_record)
+        # так як ключ до record в ADRESS_BOOK це record.name.value то я не можу просто змінити self.name
+        del ADDRESS_BOOK[self.name.value]
+        # бо вийде так, що доступ до контакну з новим name буде тільки по старому name.
+        return "Name successfully changed."
+
+    def change_phone(self):
+        old_phone = input("Enter phone that you want to edit\n")
+        index = self.phone.value.index(old_phone)
+        if not old_phone in self.phone.value:
+            return "Phone not found"
+
+        new_phone = input("Enter new phone\n")
+
+        if Phone.phone_validator(new_phone):
+            self.phone.value.pop(index)
+            self.phone.value.append(new_phone)
+            return f"Phone successfully changed from {old_phone} --> {new_phone}"
+        else:
+            return "Incorrect phone"
+
+    def change_birthday(self):
+
+        new_birthday = input("Enter new birthday\n")
+
+        if Birthday.birthday_validator(new_birthday):
+            self.birthday = Birthday(new_birthday)
+            return "Birthday successfully changed."
+        else:
+            return "Incorrect birthday"
+
+    def change_adress(self):
+
+        new_adress = Adress(input("Enter new adress\n"))
+        self.adress = new_adress
+        return "Adress successfully changed."
+
+    def change_email(self):
+
+        new_email = input("Enter new email\n")
+
+        if Email.email_validator(new_email):
+            self.email = new_email
+            return "Email successfully changed."
+        else:
+            return "Incorrect email"
+
+
+class RecordLoadFile():
+    def record_book_to_file(self):
+        pass
+
+    def read_book_from_file(self):
+        pass
+
+
+class AdressBook(UserDict, RecordLoadFile):
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+
+class NoteBook(UserDict, RecordLoadFile):
+    def add_note(self, notification: Notification):
+        self.data[notification.tags.value] = notification
+
+
+NOTE_BOOK = NoteBook()
+ADDRESS_BOOK = AdressBook()
---
+++ personal_helper/personal_helper/functions.py
@@ -0,0 +1,203 @@
+from datetime import datetime as dt
+from re import fullmatch
+
+from classes import (
+    ADDRESS_BOOK,
+    NOTE_BOOK,
+    Adress,
+    Birthday,
+    Email,
+    Name,
+    NoteText,
+    Notification,
+    Phone,
+    Record,
+    Tag,
+)
+
+
+def hello_message():
+    print("How can I help you?")
+
+
+# + Готово, працює
+# Створення контакту
+def create_contact():
+    name = input("Enter the name of the new contact\n")
+    if len(name) == 0:
+        print('The "name" field cannot be empty')
+        return False
+    if name in ADDRESS_BOOK:
+        print("Such a contact already exists")
+    else:
+        name_obj = Name(name)
+        phone = input(f"Enter the phone number for {name}\n")
+        if Phone.phone_validator(phone):
+            phone_obj = Phone(phone)
+            new_contact = Record(name=name_obj, phone=phone_obj)
+            ADDRESS_BOOK.add_record(new_contact)
+            print(f"Contact {name.capitalize()} with phone {phone} added!")
+        else:
+            print("The number should not contain letters or the number is too long!")
+
+
+# + Готово, працює
+# Додавання телефону
+def add_phone():
+    name = input("For which contact should I add another number?\n")
+    if name not in ADDRESS_BOOK:
+        print("No such contact exists!")
+    else:
+        new_phone = input(f"Enter new phone number for {name.capitalize()}\n")
+        if Phone.phone_validator(new_phone):
+            if new_phone not in ADDRESS_BOOK[name].phone.value:
+                ADDRESS_BOOK[name].phone.value.append(new_phone)
+                print(
+                    f"Phone number {new_phone} has been added to contact {name.capitalize()}"
+                )
+            else:
+                print("The number is duplicated")
+        else:
+            print("Invalid number")
+
+
+# + Готово, працює
+# Додавання адреси
+def add_adress():
+    name = input("For which contact should I add an address?\n")
+    if name not in ADDRESS_BOOK:
+        print("No such contact exists!")
+    else:
+        address = input(f"Add address for {name.capitalize()}\n")
+        address_obj = Adress(address)
+        ADDRESS_BOOK[name].adress = address_obj
+        print(f"Address {address} is added for contact {name.capitalize()}")
+
+
+# + Готово, працює
+# додавання імейлу
+def add_email():
+    name = input("For which contact should I add e-mail?\n")
+    if name not in ADDRESS_BOOK:
+        print("No such contact exists!")
+    else:
+        email = input(f"Enter e-mail for {name.capitalize()}\n")
+        if Email.email_validator(email):
+            email_obj = Email(email)
+            ADDRESS_BOOK[name].email = email_obj
+            print(
+                f"E-mail {email} has been added to contact {name.capitalize()}")
+        else:
+            print("Invalid e-mail")
+
+
+# + Готово, працює
+# додавання дня народження
+def add_birthday():
+    name = input("For which contact should I add a date of birth?\n")
+    if name not in ADDRESS_BOOK:
+        print("No such contact exists!")
+    else:
+        birthday = input("Enter YYYY/MM/DD" "\n").split("/")
+        person_birthday = dt(
+            year=int(birthday[0]), month=int(birthday[1]), day=int(birthday[2])
+        )
+        person_birthday = dt.date(person_birthday)
+        if Birthday.birthday_validator(person_birthday):
+            birthday_obj = Birthday(person_birthday)
+            ADDRESS_BOOK[name].birthday = birthday_obj
+            print(
+                f"Date of birth {person_birthday} added for contact {name.capitalize()}"
+            )
+        else:
+            print("Invalid date")
+
+
+# виводити список контактів, у яких день народження через
+# задану кількість днів від поточної дати;
+def show_bday_names():
+    pass
+
+
+# здійснювати пошук контактів з книги контактів;
+def searcher_people():
+    pass
+
+
+# редагування контакту
+def edit_contact():
+    name = input("For which contact I should edit\n")
+    if name not in ADDRESS_BOOK.data:
+        print("No such contact exists!")
+    else:
+        record = ADDRESS_BOOK[name]
+
+        parameters_to_edit = {
+            "name": record.change_name,
+            "phone": record.change_phone,
+            "birthday": record.change_birthday,
+            "adress": record.change_adress,
+            "email": record.change_email,
+        }
+        parameter = input(
+            "What you want to edit (name, phone, birthday, adress, email)\n"
+        )
+
+        if parameter in parameters_to_edit:
+            output = parameters_to_edit[parameter]()
+            print(output)
+        else:
+            print("Incorrect parameter")
+
+
+# видаляти записи з книги контактів
+def delete_contact():
+    pass
+
+
+
+# + готово, працює
+# Вивід всіх контактів, з усією наявною інформацією (окрім нотаток)
+def show_all():
+    for records in ADDRESS_BOOK.values():
+        print(records.name.value.capitalize())
+        print(f"\t{records.phone.value}")
+        if records.email:
+            print(f"\t{records.email.value}")
+        if records.adress:
+            print(f"\t{records.adress.value}")
+        if records.birthday:
+            print(f"\t{records.birthday.value}")
+
+
+def notifications():
+    action = input(
+        "Actions with notes:\nEnter 1 to add a note\nEnter 2 to delete note\nEnter 3 to show all notes\n Enter 4 to edit note text\nEnter 5 to search for notes"
+    )
+    if action == "1":
+        text_note = input("Enter note text\n...")
+        text_note_obj = NoteText(text_note)
+        tags = input(
+            "Not necessary. Enter tags for the note. (can be several, separated by commas)\n..."
+        )
+        if len(tags) == 0:
+            tegs_obj = Tag("NoneTag")
+        if not " " in tags and len(tags) > 0 or "," in tags:
+            tags_list = [tag.strip() for tag in tags.split(',')]
+            tags_list.sort()
+            tegs_obj = Tag(str(tags_list))
+        if " " in tags and not "," in tags:
+            print("Separated tags by commas")
+            return False
+
+        note_record = Notification(notes=text_note_obj, tags=tegs_obj)
+        NOTE_BOOK.add_note(note_record)
+        print(f'Added note "{text_note}"')
+
+    if action == '2':
+        note =  input('Which note do you want to delete? Enter text that you want to be deleted\n')
+        for notes in NOTE_BOOK.values():
+            pass
+    if action == "3":
+        for notes in NOTE_BOOK.values():
+            print(f"{notes.tags.value}\n\t{notes.notes.value}")
